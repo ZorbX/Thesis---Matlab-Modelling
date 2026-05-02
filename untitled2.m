68a94d87faffb4550149721e1dc1d9a680515573
jedidah1 <67008177+jedidah1@users.noreply.github.com>
2026-05-02 11:13:37 +0200

    new files

--- untitled2.m
+++ untitled2.m
@@ -1,3 +1,25 @@
 %this is a trial
 %this is a 6 step BLDC motor control model to read about 
 %openExample('mcb/SixStepBLDCQepHallExample','supportingFile','mcb_bldc_sixstep_f28069mLaunchPad.slx')
+
+%building motor parameters database 
+V = 36;%nominal voltage 
+w0 = 5690; %no load speed in rpms
+i0 = 709; % no loa current in mA
+wn = 4860; %nominal speed in rpms
+Tn = 1390; %Nominal torque (max continous torque) in mNm
+in = 21.2; %Nominal current (max continous current) in A
+
+R = 0.123; % Terminal Resistance phase to phase in ohms
+L = 0.0799; % terminal Inductance phase to phase in henries
+K_t = 59.9; % Torque constant in mNm/A
+
+J = 0.740; % Moment of inertia in kg*m^2
+w_t = 159; %speed constant in rpm/V
+P = 14; %number of pole pairs
+w_max = 8000; %max speed 
+T = 8.4; %Thrust in Kg
+flx = 60/(sqrt(3)*pi*w_t*P);% flux linkage in webbers
+K_e = 60/(2*pi*w_t); % Back EMF constant in V/(rad/s)
+
+Vc= ((i0*K_t)/1000); %vicosity friction coefficient
